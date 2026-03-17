aad09339bf5f7c4a89ce7db1ed5e17bcfcab5735
mzengin016-ux <mzengin016@gmail.com>
2026-03-17 20:45:50 +0300

    Update server.js

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.post('/api/events', (req, res) => {
   }
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Iran Crisis Map Event Server running on http://localhost:${PORT}`);
+  console.log(`Iran Crisis Map Event Server running on port ${PORT}`);
 });
